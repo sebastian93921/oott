30f124235c76498dcd76db3f457713b1c3c1009f
sebastian93921 <sebastian93921@gmail.com>
2023-06-10 02:33:35 +0800

    Add subdomain prefix combination and basic screening

--- subdomains/massdns.go
+++ subdomains/massdns.go
@@ -11,10 +11,13 @@ import (
 	"os/signal"
 	"strings"
 	"syscall"
+	"time"
 )
 
 type Massdns struct {
 	ScannedSubdomains []SubDomainDetails
+	TargetDomain      string
+	FalsePositiveHost map[string]int
 }
 
 var massdnsCommand = "massdns"
@@ -22,6 +25,7 @@ var tmpfolder = "/tmp"
 
 func (s *Massdns) ScanSubdomains(domain string) ([]SubDomainDetails, error) {
 	fmt.Println("[+] Scanning subdomains on Massdns:", domain)
+	s.TargetDomain = domain
 
 	wordlistFilePath := tmpfolder + "/subdomains-prefix.txt"
 	resolversFilePath := tmpfolder + "/dns-resolvers.txt"
@@ -38,8 +42,11 @@ func (s *Massdns) ScanSubdomains(domain string) ([]SubDomainDetails, error) {
 		return nil, nil
 	}
 
-	// Download the file
-	err = downloadFile(wordlist, wordlistFilePath)
+	if isFastScan {
+		err = downloadFile(wordlist_long, wordlistFilePath)
+	} else {
+		err = downloadFile(wordlist, wordlistFilePath)
+	}
 	if err != nil {
 		fmt.Println("[!] Error downloading file:", err)
 		return nil, nil
@@ -66,10 +73,47 @@ func (s *Massdns) ScanSubdomains(domain string) ([]SubDomainDetails, error) {
 
 	var subdomainsString []string
 
-	// Iterate over the subdomain prefixes
-	for _, prefix := range subdomainPrefixes {
-		subdomain := fmt.Sprintf("%s.%s", prefix, domain)
-		subdomainsString = append(subdomainsString, subdomain)
+	if isFastScan {
+		// Iterate over the subdomain prefixes
+		for _, prefix := range subdomainPrefixes {
+			subdomain := fmt.Sprintf("%s.%s", prefix, domain)
+			subdomainsString = append(subdomainsString, subdomain)
+		}
+	} else {
+		// Create combinations for subdomain prefix
+		fmt.Println("[+] Generating combination of prefix...")
+		var subdomainPrefixesNew []string
+		totalCombinations := len(subdomainPrefixes) * len(subdomainPrefixes) * 2
+		combinationsGenerated := 0
+		generateProcessFinished := false
+		// Start a separate goroutine to print progress every 5 seconds
+		go func() {
+			ticker := time.NewTicker(5 * time.Second)
+			defer ticker.Stop()
+
+			for range ticker.C {
+				fmt.Printf("[-] Generating combination of prefix, please wait... %d/%d\n", combinationsGenerated, totalCombinations)
+				if generateProcessFinished {
+					return
+				}
+			}
+		}()
+		for _, prefix := range subdomainPrefixes {
+			for _, prefix2 := range subdomainPrefixes {
+				newprefix1 := fmt.Sprintf("%s-%s", prefix, prefix2)
+				subdomainPrefixesNew = append(subdomainPrefixesNew, prefix, newprefix1)
+				combinationsGenerated += 2
+			}
+		}
+		generateProcessFinished = true
+		// Wait for a short duration to allow the last progress update to be printed
+		time.Sleep(1 * time.Second)
+
+		// Iterate over the subdomain prefixes
+		for _, prefix := range subdomainPrefixesNew {
+			subdomain := fmt.Sprintf("%s.%s", prefix, domain)
+			subdomainsString = append(subdomainsString, subdomain)
+		}
 	}
 
 	// Call the MassDNS command with the subdomain
@@ -144,11 +188,7 @@ func (s *Massdns) runMassDNS(resolversFilePath string, subdomains []string) erro
 
 	fmt.Println("[+] Size of subdomains generated: ", len(subdomains), " . Start running, please wait..")
 	fmt.Println("[+] Press Ctrl+C to cancel this operation if it doesn't produce any results for an extended period of time.")
-	err := s.runMassDNSByType(resolversFilePath, subdomains, "CNAME")
-	if err != nil {
-		return err
-	}
-	err = s.runMassDNSByType(resolversFilePath, subdomains, "A")
+	err := s.runMassDNSByType(resolversFilePath, subdomains, "A")
 	if err != nil {
 		return err
 	}
@@ -160,8 +200,101 @@ func (s *Massdns) runMassDNS(resolversFilePath string, subdomains []string) erro
 	return nil
 }
 
+func (s *Massdns) runRootScan(resolversFilePath string, domaintype string) []string {
+	s.FalsePositiveHost = make(map[string]int)
+	// Run a command
+	cmd := exec.Command(massdnsCommand, "-r", resolversFilePath, "-t", domaintype, "-o", "J", "-q")
+
+	// Create an io.Reader from the string
+	reader := strings.NewReader(s.TargetDomain)
+	cmd.Stdin = reader
+
+	// Create a pipe for reading the command's output
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		fmt.Printf("[!] Failed to create pipe: %v\n", err)
+		return nil
+	}
+
+	// Start the command
+	if err := cmd.Start(); err != nil {
+		fmt.Printf("[!] Failed to start command: %v\n", err)
+		return nil
+	}
+
+	// Create a scanner to read from the command's output
+	scanner := bufio.NewScanner(stdout)
+
+	// Read the output line by line in real time
+	for scanner.Scan() {
+		select {
+		case <-cancel:
+			// Scanner canceled, exit the loop
+			fmt.Println("[+] Cancel sign received, exiting..")
+			break
+		default:
+			line := scanner.Text()
+			// Parse JSON
+			var result map[string]interface{}
+			err := json.Unmarshal([]byte(line), &result)
+			if err != nil {
+				fmt.Printf("[!] Failed to parse JSON: %v\n", err)
+				continue
+			}
+
+			status := result["status"].(string)
+
+			// Print the extracted values
+			if status == "NOERROR" {
+				// Access the "data" field
+				data := result["data"].(map[string]interface{})
+
+				// Access the "answers" field within "data"
+				answers, ok := data["answers"].([]interface{})
+				if !ok {
+					continue
+				}
+				// Iterate over the answers
+				var addresses []string
+				for _, answer := range answers {
+					answerMap := answer.(map[string]interface{})
+					answerType := answerMap["type"].(string)
+
+					if answerType == domaintype {
+						addresses = append(addresses, answerMap["data"].(string))
+					}
+				}
+
+				return addresses
+
+			}
+		}
+	}
+
+	// Check if there was any error in scanning
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("[!] Failed to read command output: %v\n", err)
+		return nil
+	}
+
+	// Wait for the command to finish
+	if err := cmd.Wait(); err != nil {
+		fmt.Printf("[!] Command execution failed: %v\n", err)
+		return nil
+	}
+
+	return nil
+}
+
 func (s *Massdns) runMassDNSByType(resolversFilePath string, subdomains []string, domaintype string) error {
-	// Type A first
+	var subDomainResult []SubDomainDetails
+	fmt.Println("[+] Starting Root Addresses scan for type", domaintype)
+	rootAddresses := s.runRootScan(resolversFilePath, domaintype)
+	fmt.Println("[-] Root addresses found: ", rootAddresses)
+
+	fmt.Println("[+] Starting Subdomain scan for type", domaintype)
+
+	// Run a command
 	cmd := exec.Command(massdnsCommand, "-r", resolversFilePath, "-t", domaintype, "-s", "500", "-o", "J", "-q")
 	// Create a concatenated string of the list elements
 	stronlyDomains := strings.Join(subdomains, "\n")
@@ -215,6 +348,7 @@ func (s *Massdns) runMassDNSByType(resolversFilePath string, subdomains []string
 			if status == "NOERROR" {
 				// Access the "data" field
 				data := result["data"].(map[string]interface{})
+				resolver := result["resolver"].(string)
 
 				// Access the "answers" field within "data"
 				answers, ok := data["answers"].([]interface{})
@@ -236,17 +370,28 @@ func (s *Massdns) runMassDNSByType(resolversFilePath string, subdomains []string
 				}
 
 				if isCorrectType {
+					if s.isFalsePositive(rootAddresses, address) {
+						// Not a valid host
+						continue
+					}
 					// fmt.Println(line)
-					fmt.Printf("[MassDNS] Name: %s, Type: %s, Status: %s, Progress: %d/%d(%d%%)\n", name, typeVal, status, scanCount, total, (scanCount * 100 / total))
+					fmt.Printf("[MassDNS] Name: %s, Type: %s, Status: %s, Progress: %d/%d(%d%%), Resolver: %s\n", name, typeVal, status, scanCount, total, (scanCount * 100 / total), resolver)
 					subdomain := SubDomainDetails{
 						DomainName: strings.TrimSuffix(name, "."),
 						Address:    address,
 						Type:       typeVal,
 						ModuleName: "Massdns",
 					}
-					s.ScannedSubdomains = append(s.ScannedSubdomains, subdomain)
+					subDomainResult = append(subDomainResult, subdomain)
 				}
 			}
+		}
+	}
+
+	// Remove the false positive again
+	for _, subdomain := range subDomainResult {
+		if !s.isFalsePositive(rootAddresses, subdomain.Address) {
+			s.ScannedSubdomains = append(s.ScannedSubdomains, subdomain)
 		}
 	}
 
@@ -263,4 +408,18 @@ func (s *Massdns) runMassDNSByType(resolversFilePath string, subdomains []string
 	}
 
 	return nil
+}
+
+func (s *Massdns) isFalsePositive(rootAddresses []string, address string) bool {
+	for _, root := range rootAddresses {
+		if root == address {
+			return false
+		}
+	}
+	s.FalsePositiveHost[address] = s.FalsePositiveHost[address] + 1
+	// If the address occurs more then 4 times but it's not from our root addresses, it is possible a false possitve
+	if s.FalsePositiveHost[address] > 4 {
+		return true
+	}
+	return false
 }
